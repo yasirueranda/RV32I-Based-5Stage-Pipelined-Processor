65da8033c6781e7e0598d276ab9dd173b97253d9
yasirueranda <129848234+yasirueranda@users.noreply.github.com>
2023-12-30 22:55:27 +0530

    Delete pipelined_withoutBranch directory

--- pipelined_withoutBranch/hazard_detection_unit.sv
+++
@@ -1,9 +0,0 @@
-module hazard_detection_unit(input wire [4:0] rs1_addr_ex,rs2_addr_ex,regwrite_addr_EX,
-						 input wire ResultSrc_EX_0,
-						 output wire stallD,stallF,flushE);
-		wire lwstallD;
-		assign lwstallD=ResultSrc_EX_0 & ((rs1_addr_ex==regwrite_addr_EX)|(rs2_addr_ex==regwrite_addr_EX));
-		assign stallD=lwstallD;
-		assign stallF=lwstallD;
-		assign flushE=lwstallD;	
-endmodule

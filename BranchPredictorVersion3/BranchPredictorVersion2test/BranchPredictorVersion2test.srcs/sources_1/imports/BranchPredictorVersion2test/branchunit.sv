6492522a7dd74f1c9524ac6e698e7b574b02c7aa
VenuriAmarasinghe <125267812+VenuriAmarasinghe@users.noreply.github.com>
2023-12-31 11:36:09 +0530

    version 3 PC not updating by 1 cycle and PC source gives wrong output

---
+++ BranchPredictorVersion3/BranchPredictorVersion2test/BranchPredictorVersion2test.srcs/sources_1/imports/BranchPredictorVersion2test/branchunit.sv
@@ -0,0 +1,27 @@
+module branchunit(input wire [3:0] flags,
+						input wire [2:0] funct3,
+						input wire branch,
+						output reg PCSrc);
+	reg one,two,three,mux_out;
+	always_comb
+		begin
+		one=funct3[0]^flags[0];									//beq,bne
+		two=funct3[0]^(flags[1]^flags[3]);					//blt,bge
+		three=funct3[0]^ (~flags[2]);							//bltu,bgeu
+		end
+	
+	always_comb begin
+    case (funct3[2:1])
+        2'b00: mux_out= one;
+        2'b01: mux_out= 1'b0;
+        2'b10: mux_out= two;
+        2'b11: mux_out= three;
+        default: mux_out = 1'b0;
+    endcase
+	end
+	
+	assign PCSrc= mux_out & branch;
+	
+endmodule
+
+//flags : {o,c,n,z}

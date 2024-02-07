bc8205285a08153c7972128cb28aeffd0d899e86
VenuriAmarasinghe <125267812+VenuriAmarasinghe@users.noreply.github.com>
2024-02-07 06:30:31 +0530

    Delete BranchPredictorVersion3 directory

--- BranchPredictorVersion3/BranchPredictorVersion2test/BranchPredictorVersion2test.srcs/sources_1/imports/BranchPredictorVersion2test/hazard_detection_unit.sv
+++
@@ -1,9 +0,0 @@
-module hazard_detection_unit(input wire [4:0] rs1_addr_ID,rs2_addr_ID,regwrite_addr_EX,
-						 input wire ResultSrc_EX_0,
-						 output wire stallD,stallF,flushE);
-		wire lwstallD;
-		assign lwstallD=ResultSrc_EX_0 & ((rs1_addr_ID==regwrite_addr_EX)|(rs2_addr_ID==regwrite_addr_EX));
-		assign stallD=lwstallD;
-		assign stallF=lwstallD;
-		assign flushE=lwstallD;	
-endmodule
